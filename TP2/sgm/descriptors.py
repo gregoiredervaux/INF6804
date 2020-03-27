3973c4001cfa79f2e24e6ab00ac38f6497f4d879
gregoire <gregoire.dervaux@2019.icam.fr>
2020-03-27 19:14:55 -0400

    descripteur hog pret

--- TP2/sgm/descriptors.py
+++ TP2/sgm/descriptors.py
@@ -5,6 +5,17 @@ import numpy as np
 from skimage.feature import hog, BRIEF
 
 
+def dense_hog(img, dsc_size, num_orientations):
+
+    rows, cols = img.shape
+    img_hog = np.zeros((rows, cols, num_orientations))
+
+    for i in range(rows):
+        for j in range(cols):
+            local_img = img[max(0, i - int(dsc_size / 2)):min(rows, i + int(dsc_size / 2)), max(0, j - int(dsc_size / 2)):min(cols, j + int(dsc_size / 2))]
+            img_hog[i,j,:] = hog(local_img, orientations=num_orientations, pixels_per_cell=(local_img.shape[0], local_img.shape[1]), cells_per_block=(1,1))
+    return img_hog
+
 def apply_hog(left, right, descriptor_size, num_orientations):
     """
     computes HOG descriptor on both images.
@@ -14,7 +25,8 @@ def apply_hog(left, right, descriptor_size, num_orientations):
     :param num_orientations: number of HOG orientations.
     :return: (H x W x M) array, H = height, W = width and M = num_orientations, of type np.float32.
     """
-    # TODO: apply HOG descriptor on left and right images.
+
+    return (dense_hog(left, descriptor_size, num_orientations), dense_hog(right, descriptor_size, num_orientations))
 
 
 def apply_brief(left, right, descriptor_size, num_elements):
